6b3b438454b27f79586f18bc554c5a06a205e702
lholmkvist01-lang <lholmkvist01@gmail.com>
2026-09-11 16:03:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE='fantasy-holle-v2';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
